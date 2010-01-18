fe55165bfc50c0986c7f4b4a51a761848b4bbddb
Dusty Phillips <dusty@archlinux.ca>
2010-01-18 16:14:56 -0600

    Rename the project folder.

--- demos/helloworld/helloworld.py
+++ demos/helloworld/helloworld.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #
 # Copyright 2009 Facebook
+# Some portions Copyright 2010 Dusty Phillips
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -14,29 +15,29 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import tornado.httpserver
-import tornado.ioloop
-import tornado.options
-import tornado.web
+import psyclone.httpserver
+import psyclone.ioloop
+import psyclone.options
+import psyclone.web
 
-from tornado.options import define, options
+from psyclone.options import define, options
 
 define("port", default=8888, help="run on the given port", type=int)
 
 
-class MainHandler(tornado.web.RequestHandler):
+class MainHandler(psyclone.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
 
 def main():
-    tornado.options.parse_command_line()
-    application = tornado.web.Application([
+    psyclone.options.parse_command_line()
+    application = psyclone.web.Application([
         (r"/", MainHandler),
     ])
-    http_server = tornado.httpserver.HTTPServer(application)
+    http_server = psyclone.httpserver.HTTPServer(application)
     http_server.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
+    psyclone.ioloop.IOLoop.instance().start()
 
 
 if __name__ == "__main__":
