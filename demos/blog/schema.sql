60463687475d42ab8a8e986bc215cabd3ab14e45
Dusty Phillips <dusty@archlinux.ca>
2010-01-18 22:39:46 -0600

    Change schema to be postgres compliant.

--- demos/blog/schema.sql
+++ demos/blog/schema.sql
@@ -1,4 +1,5 @@
 -- Copyright 2009 FriendFeed
+-- Some Changes Copyright 2010 Dusty Phillips
 --
 -- Licensed under the Apache License, Version 2.0 (the "License"); you may
 -- not use this file except in compliance with the License. You may obtain
@@ -13,32 +14,27 @@
 -- under the License.
 
 -- To create the database:
---   CREATE DATABASE blog;
---   GRANT ALL PRIVILEGES ON blog.* TO 'blog'@'localhost' IDENTIFIED BY 'blog';
+-- $ creatuser psycloneblog
+-- $ createdb -U psycloneblog psycloneblog
 --
 -- To reload the tables:
---   mysql --user=blog --password=blog --database=blog < schema.sql
+--   psql -U psycloneblog psycloneblog < schema.sql
 
-SET SESSION storage_engine = "InnoDB";
-SET SESSION time_zone = "+0:00";
-ALTER DATABASE CHARACTER SET "utf8";
+DROP TABLE IF EXISTS authors CASCADE;
+CREATE TABLE authors (
+    id SERIAL NOT NULL PRIMARY KEY,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL
+);
 
 DROP TABLE IF EXISTS entries;
 CREATE TABLE entries (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     author_id INT NOT NULL REFERENCES authors(id),
     slug VARCHAR(100) NOT NULL UNIQUE,
     title VARCHAR(512) NOT NULL,
-    markdown MEDIUMTEXT NOT NULL,
-    html MEDIUMTEXT NOT NULL,
-    published DATETIME NOT NULL,
-    updated TIMESTAMP NOT NULL,
-    KEY (published)
-);
-
-DROP TABLE IF EXISTS authors;
-CREATE TABLE authors (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    name VARCHAR(100) NOT NULL
+    markdown TEXT NOT NULL,
+    html TEXT NOT NULL,
+    published TIMESTAMP NOT NULL,
+    updated TIMESTAMP NOT NULL
 );
